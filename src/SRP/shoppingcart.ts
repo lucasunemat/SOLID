cc4d37ec4e003ee61ec8eef4e1dd0dbe376d5c19
Lucas Nunes Batista <lucas.batista@unemat.br>
2023-11-17 19:18:57 -0400

    finalização carrinho sem SOLID

--- src/SRP/shoppingcart.ts
+++ src/SRP/shoppingcart.ts
@@ -1,13 +1,14 @@
 /*eslint-disable*/
 
 type CartItem = { name: string; price: number };
+type OrderStatus = 'open' | 'closed';
 
 export class ShoppingCart {
   //array de objetos com atributos name e price
   //o array começa vazio
   private readonly _items: CartItem[] = [];
   //cria um status que pode ser 'open' ou 'closed' e inicia em 'open'
-  private orderStatus: 'open' | 'closed' = 'open';
+  private _orderStatus: OrderStatus = 'open';
 
   addItem(item: CartItem): void {
     this._items.push(item);
@@ -23,6 +24,10 @@ export class ShoppingCart {
     return this._items;
   }
 
+  get orderStatus(): OrderStatus {
+    return this._orderStatus;
+  }
+
   total(): number {
     return parseFloat(this._items
       .reduce((total, next) => total + next.price, 0)
@@ -30,7 +35,32 @@ export class ShoppingCart {
   }
 
   checkout(): void {
+    if (this.isEmpty()) {
+      console.log('Seu carrinho está vazio');
+      return;
+    }
 
+    this._orderStatus = 'closed';
+    this.sendMessage(`Seus pedido com total de ${this.total()} foi recebido!!`);
+    this.saveOrder();
+    this.clear();
+  }
+
+  sendMessage(msg: string): void {
+    console.log('Mensagem enviada: ', msg);
+  }
+
+  saveOrder(): void {
+    console.log('Pedido salvo com sucesso!');
+  }
+
+  clear(): void {
+    console.log('Carrinho de compras foi limpo!');
+    this._items.length = 0;
+  }
+
+  isEmpty(): boolean {
+    return this._items.length === 0; //validar se carrinho tá vazio
   }
 }
 
@@ -40,4 +70,6 @@ shoppingCart.addItem({ name: 'Bermuda', price: 79.91 });
 shoppingCart.addItem({ name: 'Tenis', price: 129.91 });
 
 console.log(shoppingCart.items);
-console.log('Total= ', shoppingCart.total());
+console.log('Status atual carrinho=', shoppingCart.orderStatus);
+shoppingCart.checkout();
+console.log('Status atual carrinho=', shoppingCart.orderStatus);
