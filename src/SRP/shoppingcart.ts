8e69684fffa72f59638f3614293348c3b64cbea0
Lucas Nunes Batista <lucas.batista@unemat.br>
2023-11-17 18:05:58 -0400

    continuação aula carrinho compras

--- src/SRP/shoppingcart.ts
+++ src/SRP/shoppingcart.ts
@@ -1,8 +1,13 @@
+/*eslint-disable*/
+
 type CartItem = { name: string; price: number };
 
 export class ShoppingCart {
   //array de objetos com atributos name e price
+  //o array começa vazio
   private readonly _items: CartItem[] = [];
+  //cria um status que pode ser 'open' ou 'closed' e inicia em 'open'
+  private orderStatus: 'open' | 'closed' = 'open';
 
   addItem(item: CartItem): void {
     this._items.push(item);
@@ -12,4 +17,27 @@ export class ShoppingCart {
     //a partir do index, remove 1 item
     this._items.splice(index, 1);
   }
+
+  get items(): Readonly<CartItem>[] {
+    //retorna uma cópia do array
+    return this._items;
+  }
+
+  total(): number {
+    return parseFloat(this._items
+      .reduce((total, next) => total + next.price, 0)
+      .toFixed(2));
+  }
+
+  checkout(): void {
+
+  }
 }
+
+const shoppingCart = new ShoppingCart();
+shoppingCart.addItem({ name: 'Camiseta', price: 49.91 });
+shoppingCart.addItem({ name: 'Bermuda', price: 79.91 });
+shoppingCart.addItem({ name: 'Tenis', price: 129.91 });
+
+console.log(shoppingCart.items);
+console.log('Total= ', shoppingCart.total());
