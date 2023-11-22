bbb1dd667a208f3bbc1f95bacbd98bbd8e0d7306
Lucas Nunes Batista <lucas.batista@unemat.br>
2023-11-21 20:16:06 -0400

    copia da pasta srp

---
+++ src/SRP copy/entities/product.ts
@@ -0,0 +1,12 @@
+/*eslint-disable*/
+
+/**
+ * Aqui temos exemplo de abstração
+ */
+
+import { CartItem } from '../interfaces/cartItem';
+
+export class Product implements CartItem{
+  constructor(public name: string, public price: number) {
+  }
+}
---
+++ src/SRP copy/entities/shoppingcart.ts
@@ -0,0 +1,46 @@
+/*eslint-disable*/
+
+import { CartItem } from '../interfaces/cartItem';
+
+export class ShoppingCart {
+
+  private readonly _items: CartItem[] = [];
+
+  addItem(item: CartItem): void {
+    this._items.push(item);
+  }
+
+  removeItem(index: number): void {
+
+    this._items.splice(index, 1);
+  }
+
+  get items(): Readonly<CartItem>[] {
+
+    return this._items;
+  }
+
+  total(): number {
+    return parseFloat(this._items
+      .reduce((total, next) => total + next.price, 0)
+      .toFixed(2));
+  }
+
+  sendMessage(msg: string): void { //potencial atributo que não é responsabilidade do carrinho
+    console.log('Mensagem enviada: ', msg);
+  }
+
+  saveOrder(): void { //potencial atributo que não é responsabilidade do carrinho
+    console.log('Pedido salvo com sucesso!');
+  }
+
+  clear(): void { //mantido porque mexe com atributo privado do carrinho
+    console.log('Carrinho de compras foi limpo!');
+    this._items.length = 0;
+  }
+
+  isEmpty(): boolean { //validação mantida por ser a única presente no carrinho
+    return this._items.length === 0;
+  }
+}
+
