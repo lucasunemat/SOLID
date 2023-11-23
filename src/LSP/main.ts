5209dd12181de9278bc068316b5e733894a024dc
Lucas Nunes Batista <lucas.batista@unemat.br>
2023-11-23 16:31:50 -0400

    final de lsp

--- src/LSP/main.ts
+++ src/LSP/main.ts
@@ -5,6 +5,7 @@
  * Se q(x) é uma propriedade demonstrável dos objetos x de tipo T. Então q(y) deve ser verdadeiro para objetos y de tipo S onde S é um subtipo de T.
  * Mais simples ainda: Subtipos precisam ser substituíveis por seus tipos de base.
  * Mais simples ainda: Se meu programa espera um Animal, algo do tipo Cachorro (que herda de Animal) deve servir como qualquer outro Animal.
+ * Se há classes derivadas de Discount, elas precisam se comportar como a classe base Discount.
  */
 
 import { ShoppingCart } from './classes/shoppingcart';
