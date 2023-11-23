ee5fc48946c2983b3f93b1e2b6943afd42fe8fcf
Lucas Nunes Batista <lucas.batista@unemat.br>
2023-11-23 11:05:50 -0400

    commit

--- src/LSP/classes/discount.ts
+++ src/LSP/classes/discount.ts
@@ -11,7 +11,17 @@
 export abstract class Discount {
   protected discount = 0; //desconto padrão: 0
 
-  calculate(price: number): number { //método funcional global
+  /*
+   * se você trocar o retorno da função de number para unknowm
+   * você vai precisar validar em cada chamada de função na main se tá retornando number ou não
+   * ou seja, você vai estar quebrando um comportamento da base ao chamar a função especifica da classe derivada da classe base
+   */
+
+  /**
+   * Outra forma de quebrar o LSP: mudar o tipo de retorno da função dentro das classes filhas abaixo
+   * Ou seja, tô mudando o retorno da função na classe filha e não na classe base
+   */
+  calculate(price: number): number {
     return price - price * this.discount;
   }
 }
@@ -20,7 +30,7 @@ export class FiftyPercentDiscount extends Discount {
   //usando discount aqui pq essa classe herda de Discount
   //agora é só ir mudando o valor de discount para mudar o desconto
   protected readonly discount = 0.5;
-
+  //o método calculate é herdado de Discount, não precisa aparecer aqui
 }
 
 export class TenPercentDiscount extends Discount {
@@ -29,5 +39,5 @@ export class TenPercentDiscount extends Discount {
 }
 
 //usando discount aqui pq essa classe herda de Discount
-export class NoDiscount extends Discount {}
+export class NoDiscount extends Discount { }
 
