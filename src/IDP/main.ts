c00437d8e50d1e55bbd68f565598b5900c0e8da8
Lucas Nunes Batista <lucas.batista@unemat.br>
2023-11-27 15:16:25 -0400

    anotações

--- src/IDP/main.ts
+++ src/IDP/main.ts
@@ -14,6 +14,7 @@
  * No caso, temos que customer em Order é uma abstração, pois é a instanciação de uma interface.
  */
 
+//imports
 import { ShoppingCart } from './classes/shoppingcart';
 import { Order } from './classes/order';
 import { Messaging } from './services/messaging';
@@ -21,23 +22,47 @@ import { Persistency } from './services/persistency';
 import { Product } from './classes/product';
 import { FiftyPercentDiscount, NoDiscount, TenPercentDiscount } from './classes/discount';
 import { EnterpriseCustomer, IndividualCustomer } from './classes/customer';
+import { MessagingProtocol } from './classes/interfaces/messaging-protocol';
 
+//descontos
 const fiftyPercentDiscount = new FiftyPercentDiscount();
 const noDiscount = new NoDiscount();
 const tenPercentDiscount = new TenPercentDiscount();
+
+//carrinho com messaging, persistency e desconto
 //const shoppingCart = new ShoppingCart(fiftyPercentDiscount);
 const shoppingCart = new ShoppingCart(tenPercentDiscount);
 const messaging = new Messaging();
 const persistency = new Persistency();
+
+//messaging teste (mock)
+
+class MessagingMock implements MessagingProtocol {
+  sendMessage() {
+    console.log('A mensagem foi enviada pelo mock');
+  }
+}
+
+const messagingMock = new MessagingMock();
+
+//configuração de cliente
 const individualCustomer = new IndividualCustomer('Lucas', 'Bertoncello', '111.111.111-11');
 const enterpriseCustomer = new EnterpriseCustomer('Empresa LiTroLândia', '222.222.222/00001-22');
-const order = new Order(shoppingCart, messaging, persistency, enterpriseCustomer);
+
+//ordem de compra
+const order = new Order(shoppingCart, messagingMock, persistency, enterpriseCustomer);
 shoppingCart.addItem(new Product('Camiseta', 49.91));
 shoppingCart.addItem(new Product('Lapis', 9.91));
 shoppingCart.addItem(new Product('Calça', 89.91));
 
+//consoles
 console.log(shoppingCart.items);
 console.log('Total = ', shoppingCart.totalWithDiscount());
 console.log('Status da ordem de compra = ', order.orderStatus);
 order.checkout();
 console.log('Status da ordem de compra = ', order.orderStatus);
+
+//veja que a Order acabou utilizando a messagingMock sem nem saber. Teste realizado!!
+
+
+
